1fd5fb38b73d412cf8166531223db4dd46bf5a23
Rafał <lama3773@gmail.com>
2017-07-12 15:57:24 +0200

    database dump

--- sql/dump.sql
+++ sql/dump.sql
@@ -27,8 +27,46 @@ CREATE TABLE `admins` (
   `user_id` int(11) NOT NULL,
   `admin_flags` int(11) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `address` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `street` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `city` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nip` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company`
+--
+
+LOCK TABLES `company` WRITE;
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` VALUES ('AdamFirma','98-765','Adamia','Adamów','Polska','8527419630','adamFirma@gmail.com'),('sad','asf','saf','asf','asf','1234567899','adaMmFirma@gmail.com'),('AniaFirma','22-999','Krakowska 45','Białystok','Polska','7894561230','aniaFirma@gmail.com'),('JanFirma','23-456','Janowska','Janowice','Polska','1234567890','janfirma@gmail.com'),('KasiaFirma','45-345','Katarzynki','Katarzów','Polska','7899511598','kasiaFirma@gmail.com'),('KingaFirma','44-966','Anielska 76','Inowrocław','Polska','5984122368','kingaFirma@gmail.com'),('MarcinFirma','44-555','Marcinka 148','Marciniec','Polska','3692581470','marcinFirma@gmail.com'),('MarekFirma','12-124','Markowa','Marków','Polska','8524796448','marekFirma@gmail.com'),('MieczysławFirma','12341','Columb Street','Chicago','USA','7895642318','MieczysławFirma@interia.pl'),('MirekFirma','12-321','Mirkowa 34','Mirków','Polska','4567891232','MierkFirma@gmail.com'),('MietekFirma','31-123','Mirkowa 34','Tarnów','Polska','8524796448','MietekFirma@gmail.com'),('nowakFirma','31-123','Nowacka','Nowogródek','Polska','3692581470','nowakFirma@gmail.com'),('RadekFirma','12-999','Radkiewicza 67','Radków','Polska','8524782155','RadekFirma@gmail.com'),('RafałFirma','33-131','Tarnowska 149','Łęg Tarnowski','Polska','1234567890','rafalFirma@gmail.com'),('RomanFirma','23-123','Romanowicza 1a','Kraków','Polska','7521469852','RomanFirma@gmail.com'),('SamFirma','123-12','Samuel Street','San Diego','Polska','1598741262','samFirma@gmail.com');
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `contacts`
@@ -45,6 +83,15 @@ CREATE TABLE `contacts` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `contacts`
+--
+
+LOCK TABLES `contacts` WRITE;
+/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -58,11 +105,22 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `session_hash` varchar(32) DEFAULT NULL,
+  `user_group` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (13,'admin','admin','admin@gmail.com','2017-07-12 12:27:39','c5bf7e4c9f28b14b868fa640f742063a',1),(14,'ktos','ktos','ktos@gmail.com','2017-07-11 12:45:45',NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -73,4 +131,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-10 12:32:25
+-- Dump completed on 2017-07-12 15:56:15
